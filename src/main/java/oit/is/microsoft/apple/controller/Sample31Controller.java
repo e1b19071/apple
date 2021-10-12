032b36eb1c36d7c6c39380a4f6f1c8dc70415d55
e1b19071 <e1b19071@oit.ac.jp>
2021-10-12 16:04:02 +0900

    sample31を表辞するリクエストを表示

---
+++ src/main/java/oit/is/microsoft/apple/controller/Sample31Controller.java
@@ -0,0 +1,38 @@
+package oit.is.microsoft.apple.controller;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * /sample3へのリクエストを扱うクラス authenticateの設定をしていれば， /sample3へのアクセスはすべて認証が必要になる
+ */
+@Controller
+@RequestMapping("/sample3")
+public class Sample31Controller {
+
+  @GetMapping("step1")
+  public String sample31() {
+    return "sample31.html";
+  }
+
+  /**
+   *
+   * @param model Thymeleafにわたすデータを保持するオブジェクト
+   * @param prin  ログインユーザ情報が保持されるオブジェクト
+   * @return
+   */
+  @GetMapping("step2")
+  public String sample32(ModelMap model, Principal prin) {
+    String loginUser = prin.getName(); // ログインユーザ情報
+    model.addAttribute("login_user", loginUser);
+    return "sample31.html";
+  }
+
+}
