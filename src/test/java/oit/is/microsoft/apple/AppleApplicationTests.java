56facd937f7d456ff506270088fadcc00229e126
Suu121006 <e1b19088@oit.ac.jp>
2021-10-12 14:14:29 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/microsoft/apple/AppleApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.microsoft.apple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
